3c18ca3fb85faf779b8e0b6b9ba80fd59f1f3d9a
Sudeep Batra <hftautobot@gmail.com>
2023-02-19 19:41:49 +0530

    Linked List Loop Detection.

--- src/main/java/com/stoxalpha/LinkedList.java
+++ src/main/java/com/stoxalpha/LinkedList.java
@@ -126,4 +126,24 @@ public class LinkedList {
             current = current.next;
         }
     }
+
+    public boolean hasLoop() {
+        return hasLoop(this.head);
+    }
+
+    private boolean hasLoop(Node first) {
+        Node slow = first;
+        Node fast = first;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next; //1 hop
+            fast = fast.next.next; //2 hop
+
+            if (slow == fast) { //fast caught up to slow, so there is a loop.
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
